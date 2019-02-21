72db1e0f2d3fafa02672a1c433d441c2ef4a86da
Dimas Restu H <dimas.restu@student.upi.edu>
2019-02-21 20:54:39 +0700

    update users controller

--- controllers/users.js
+++ controllers/users.js
@@ -9,7 +9,7 @@ function index(req, res) {
   // Parse JWT Claims from Header
   let dataClaims = jwt.getClaims(res.get('X-JWT-Claims'))
   
-  // Response with JWT Data
+  // Response with JWT Claims
   response.resSuccessData(res, dataClaims.data)
 }
 
